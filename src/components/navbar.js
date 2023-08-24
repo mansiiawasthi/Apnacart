827fa9f100be384772b8f00c3680a9ae7b6afcf2
Mansi Awasthi <112656051+mansiiawasthi@users.noreply.github.com>
2023-08-24 23:09:55 +0530

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,17 +1,30 @@
+// import React from "react";
+
+// class Navbar extends React.Component {
+//   render() {
+//     return (
+//       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+//         <div className="container-fluid">
+//           <a className="navbar-brand" href="#">
+//             Apna Cart
+//           </a>
+//         </div>
+//       </nav>
+//     );
+//   }
+// }
+
+// export default Navbar;
 import React from "react";
 
-class Navbar extends React.Component {
-  render() {
-    return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            Apna Cart
-          </a>
-        </div>
-      </nav>
-    );
-  }
+function Navbar() {
+  return (
+    <nav>
+      <button>Home</button>
+      <button>Products</button>
+      <button>Contact</button>
+    </nav>
+  );
 }
 
 export default Navbar;
